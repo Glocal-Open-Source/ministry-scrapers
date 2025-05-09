8bd23ff481eb5ee39b62f676bd3ab8b33a046cca
Daniel Biel <daniel.biel@glocalfoundation.ca>
2025-05-09 13:32:45 -0400

    BC config created

---
+++ scrapers/bc/config.py
@@ -0,0 +1,133 @@
+# BC config
+
+# urls for:
+# Role
+# Attorney General
+# Ministers
+# Deputy Ministers
+# Deputy Attorney Generals
+
+#          fields
+#  -----------------------
+# | Type              str |
+# | Ministry          str |
+# | Role              str |
+# | Honorable         bool|
+# | Name              str |
+# | French            bool|
+# | Biography         str |
+# | Contact URL       str |
+# | Contact Phone     str |
+# | Office            str |
+#  -----------------------
+#
+
+urls = {
+
+    "Ministry of Agriculture and Food": [
+        "https://news.gov.bc.ca/ministries/agriculture-and-food/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-for-agriculture",
+    ],
+
+    "Attorney General": [
+        "https://news.gov.bc.ca/ministries/attorney-general/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-for-anti-racism-initiatives"
+    ],
+
+    "Children and Family Development": [
+        "https://news.gov.bc.ca/ministries/children-and-family-development/biography",
+    ],
+
+    "Citizens' Services": [
+        "https://news.gov.bc.ca/ministries/citizens-services/biography"
+    ],
+
+    "Education and Child Care": [
+        "https://news.gov.bc.ca/ministries/education-and-child-care/child-care/biography"
+    ],
+
+    "Emergency Management and Climate Readiness": [
+        "https://news.gov.bc.ca/ministries/emergency-management-and-climate-readiness/biography",
+    ],
+
+    "Energy and Climate Solutions": [
+        "https://news.gov.bc.ca/ministries/energy-and-climate-solutions/biography",
+    ],
+
+    "Environment and Parks": [
+        "https://news.gov.bc.ca/ministries/environment-and-parks/biography",
+    ],
+
+    "Finance": [
+        "https://news.gov.bc.ca/ministries/finance/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/finance"
+    ],
+
+    "Forests": [
+        "https://news.gov.bc.ca/ministries/forests/biography",
+    ],
+
+    "Health": [
+        "https://news.gov.bc.ca/ministries/health/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/health"
+    ],
+
+    "Housing and Municipal Affairs": [
+        "https://news.gov.bc.ca/ministries/housing-and-municipal-affairs/biography",
+    ],
+
+    "Indigenous Relations and Reconciliation": [
+        "https://news.gov.bc.ca/ministries/indigenous-relations-and-reconciliation/biography",
+    ],
+
+    "Infrastructure": [
+        "https://news.gov.bc.ca/ministries/infrastructure/biography",
+    ],
+
+    "Jobs, Economic Development and Innovation": [
+        "https://news.gov.bc.ca/ministries/jobs-economic-development-and-innovation/biography",
+    ],
+
+    "Labour": [
+        "https://news.gov.bc.ca/ministries/labour/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/labour"
+    ],
+
+    "Mining and Critical Minerals": [
+        "https://news.gov.bc.ca/ministries/mining-and-critical-minerals/biography",
+    ],
+
+    "Post-Secondary Education and Future Skills": [
+        "https://news.gov.bc.ca/ministries/post-secondary-education-and-future-skills/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-for-international-credentials"
+    ],
+
+    "Public Safety and Solicitor General": [
+        "https://news.gov.bc.ca/ministries/public-safety-and-solicitor-general/biography",
+        "https://news.gov.bc.ca/ministries/community-safety-and-integrated-services/biography"
+    ],
+
+    "Social Development and Poverty Reduction": [
+        "https://news.gov.bc.ca/ministries/social-development-and-poverty-reduction/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-accessibility",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-community-development-non-profits"
+    ],
+
+    "Tourism, Arts, Culture and Sport": [
+        "https://news.gov.bc.ca/ministries/tourism-arts-culture-and-sport/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-arts-film"
+    ],
+
+    "Trade": [
+        "https://news.gov.bc.ca/ministries/trade/biography",
+    ],
+
+    "Transportation and Transit": [
+        "https://news.gov.bc.ca/ministries/transportation-and-transit/biography",
+        "https://www2.gov.bc.ca/gov/content/governments/organizational-structure/cabinet/cabinet-ministers/parliamentary-secretary-for-transit"
+    ],
+
+    "Water, Land and Resource Stewardship": [
+        "https://news.gov.bc.ca/ministries/water-land-and-resource-stewardship/biography",
+    ]
+}
